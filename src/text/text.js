fb7b72bd9838394369fd8b90d8f638e96b44977b
Rob Majuri <robmajuri@gmail.com>
2019-04-06 11:27:26 -0400

    working on formatting quotes

---
+++ src/text/text.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './text.module.css'
+
+const Text = () => {
+ 
+  return (
+      <div>
+    <h1>The Cask of Amontillado</h1>
+    </div>
+  )
+}
+
+export default Text
