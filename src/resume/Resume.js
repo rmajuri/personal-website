89e0bd7f657b6154dc460d9cf1eeb304a22d580e
Rob Majuri <robmajuri@gmail.com>
2019-05-07 22:51:07 -0400

    pdf mobile visible. have to change size for mobile

--- src/resume/Resume.js
+++ src/resume/Resume.js
@@ -3,6 +3,8 @@ import TextHeader from '../about/TextHeader/TextHeader'
 import NavOptionsHeader from '../about/NavOptions/NavOptionsHeader'
 import { Link } from 'react-router-dom'
 import style from './Resume.module.css'
+import { Document, Page, pdfjs } from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume = () => {
   const headerClasses = [style.header, style.hvrgrow]
@@ -20,7 +22,13 @@ const Resume = () => {
             <i className="fas fa-arrow-alt-circle-down" />
           </a>
         </div>
-        <embed src="Robert_Majuri_Resume.pdf" type="application/pdf" className={style.pdf} />
+        {/* <embed src="Robert_Majuri_Resume.pdf" type="application/pdf" className={style.pdf} /> */}
+        <Document 
+        loading=""
+        file="Robert_Majuri_Resume.pdf" 
+        className={style.pdf}>
+        <Page pageNumber={1} width={800}/>
+        </Document>
       </div>
     </div>
   )
