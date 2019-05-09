b503defcd844daf22d2d120f9611d325a55f4950
Rob Majuri <robmajuri@gmail.com>
2019-05-09 15:36:48 -0400

    Updates

---
+++ precache-manifest.22b8afb7846d2ce7fb49548826b8672c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42fa79f201a82c893d86",
+    "url": "/robert-majuri/static/js/runtime~main.a4e3cdad.js"
+  },
+  {
+    "revision": "ca54f34e6ec6c80a44f8",
+    "url": "/robert-majuri/static/js/main.b0e0a407.chunk.js"
+  },
+  {
+    "revision": "f07f2fa19a8b6e1ca79d",
+    "url": "/robert-majuri/static/js/2.03a9f878.chunk.js"
+  },
+  {
+    "revision": "ca54f34e6ec6c80a44f8",
+    "url": "/robert-majuri/static/css/main.61bdc408.chunk.css"
+  },
+  {
+    "revision": "adbf755596c52296925474daa8601750",
+    "url": "/robert-majuri/index.html"
+  }
+];
