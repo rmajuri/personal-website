32aefc1dff51ccaa88932980de153a6e583a917a
Rob Majuri <robmajuri@gmail.com>
2019-05-09 16:27:17 -0400

    Updates

--- precache-manifest.58efb35e202ecd0b9c3be90a22bb8d9d.js
+++ precache-manifest.58efb35e202ecd0b9c3be90a22bb8d9d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/robert-majuri/static/js/runtime~main.a4e3cdad.js"
   },
   {
-    "revision": "6e9c6ee780b2d571c6b8",
-    "url": "/robert-majuri/static/js/main.a2c977fd.chunk.js"
+    "revision": "2bab37d66e26bef7a826",
+    "url": "/robert-majuri/static/js/main.33bf2320.chunk.js"
   },
   {
     "revision": "f07f2fa19a8b6e1ca79d",
     "url": "/robert-majuri/static/js/2.03a9f878.chunk.js"
   },
   {
-    "revision": "6e9c6ee780b2d571c6b8",
-    "url": "/robert-majuri/static/css/main.61bdc408.chunk.css"
+    "revision": "2bab37d66e26bef7a826",
+    "url": "/robert-majuri/static/css/main.1d1991c3.chunk.css"
   },
   {
-    "revision": "ef08f6a83673c918277ecf5cb54c92c9",
+    "revision": "4e507d335b91fa0da03a5def01b086db",
     "url": "/robert-majuri/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robert-majuri/precache-manifest.2a67ee2deed8ccdde20ffca186575a51.js"
+  "/robert-majuri/precache-manifest.58efb35e202ecd0b9c3be90a22bb8d9d.js"
 );
 
 workbox.clientsClaim();
