9fd4e30c5f3f07152823af732e4e02e89b443b1b
Rob Majuri <robmajuri@gmail.com>
2019-05-09 15:32:51 -0400

    getting ready to run on github pages

--- src/home/Name/Name.js
+++ src/home/Name/Name.js
@@ -4,7 +4,7 @@ import style from './Name.module.css'
 const Name = () => (
   <div className={style.container}>
     <div className={style.textContainer}>
-      <h2 className={style.headingText}>ROB MAJURI</h2>
+      <h2 className={style.headingText}>ROBERT MAJURI</h2>
       <h2 className={style.headingText}>SOFTWARE DEVELOPER</h2>
     </div>
     <div className={style.githubIcon}>
