569aa03ce197c9461cf1131bfcc41aafd6c1f645
Rob Majuri <robmajuri@gmail.com>
2019-06-01 20:36:44 -0400

    adjust font

--- src/about/About.js
+++ src/about/About.js
@@ -21,6 +21,8 @@ const About = () => {
           alt="Photograph of Rob Majuri"
         />
         <section className={style.bio}>
+        <p style={{fontWeight: 700}}>HI THERE.</p>
+        <p>I&apos;m Rob.</p>
         <p>I&apos;ve always had a restless urge for creativity and analytical thinking.</p>
         <p>Whether teaching myself how to write <a href="https://soundcloud.com/theimpressionistmusic/tracks" target="blank">music</a> or how to write a <a href="https://www.goodreads.com/book/show/33786526-lucy-teaches-in-dolores" target="blank">novel</a>, I love building something new.</p>
         <p>This urge to create and problem-solve is what drove me to pursue a career in software development.
