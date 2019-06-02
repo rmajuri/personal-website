576fe630dac62eb88e6b4124c7eb46f637534bf3
Rob Majuri <robmajuri@gmail.com>
2019-06-02 14:01:51 -0400

    adjusted color and position for greater accessibility

--- src/about/About.js
+++ src/about/About.js
@@ -31,7 +31,7 @@ const About = () => {
         <p>After teaching myself the fundamentals of JavaScript, HTML, and CSS, I attended Fullstack Academy of Code, based in NY, for 4 months.</p>
         <p>There I learned how to write applications from front to back using JavaScript, Node.js, React, Redux, PostgreSQL, and more.</p>
         <p>At my current job, I taught myself how to write Python so that I could contribute to the codebase. And lately, I&apos;ve been excited about the framework <a href="https://www.gatsbyjs.org/" target="blank">Gatsby</a>.</p>
-        <p>I like using tools that allow for clean but also enable me create pleasant user experiences.</p>
+        <p>I like using tools that allow for clean code but also enable me create pleasant user experiences.</p>
         <p>The most rewarding part of development is getting to work on projects that help people and businesses reach their goals.</p>
         <p>When I&apos;m not writing code, I like making noise in GarageBand, nerding out by talking about or reading books, and exploring the historic parts of Philadelphia.</p>
         <p>Let it be known that I am an unashamed cat enthusiast, with the calendar on my desk to prove it.</p>
