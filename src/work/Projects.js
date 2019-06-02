c89733229313ade41a34a0fc9690ffb6ea5161c9
Rob Majuri <robmajuri@gmail.com>
2019-06-02 16:36:13 -0400

    photo adjustments

--- src/work/Projects.js
+++ src/work/Projects.js
@@ -3,7 +3,7 @@ export const projectData = [
     projectTitle: 'Click-Chord',
     deployedAt: 'http://clickchord.herokuapp.com',
     github: 'https://github.com/rmajuri/click-chord',
-    tech: 'React, Tone.js, Node, Express, Webpack',
+    tech: 'JavaScript, React, Tone.js, Node, Express, Webpack',
     description: `Click-Chord offers users the ability to create synthesizer chord progressions in the browser.
     Users can choose between a "Rythm Maker" and "Rhythm Builder" to backdrop their chord progressions with percussion.`,
     note: `*Note: Not compatible with some web browsers. Best viewed using
@@ -25,7 +25,7 @@ export const projectData = [
     projectTitle: 'Catify',
     deployedAt: '',
     github: 'https://github.com/S-E-R-J/catify',
-    tech: 'React Native, Redux, Firebase',
+    tech: 'JavaScript, React Native, Redux, Firebase',
     description: `I collaborated with three other developers to create a mobile messaging app called Catify. To build this app, we used React Native and the Expo.io toolchain. Among my particular contributions to the project were developing the UI, architecting the navigation flow, and integrating Redux.`,
     note: '',
     videoURL: 'https://www.youtube.com/embed/RogIJkblx04',
